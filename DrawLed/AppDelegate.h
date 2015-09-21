6173e0298b10b01b8a9a0eb0bc45decc7fc59f0b
Dzung Tran <xuandung.hp.vn@gmail.com>
2015-09-21 10:45:04 +0700

    Initial Commit

---
+++ DrawLed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawLed
+//
+//  Created by Dzung Tran on 9/21/15.
+//  Copyright © 2015 Dzung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawLed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawLed
+//
+//  Created by Dzung Tran on 9/21/15.
+//  Copyright © 2015 Dzung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
